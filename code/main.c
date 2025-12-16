e0fc0c9f79775f40b067c0bb34b997cbf50fc302
Daniel Fairchild <daniel@fairchild.dk>
2025-12-16 12:44:06 +0100

    added qfont output to screen

--- code/main.c
+++ code/main.c
@@ -6,7 +6,7 @@ typedef struct {
   uint32_t expected_controllers;
 } rumble_test_state_t;
 
-void main_mode_updater(void *data) {
+void render(void *data) {
   rumble_test_state_t *rt_state = (rumble_test_state_t *)data;
   maple_device_t **rumblers = enj_rumbler_get_states();
   enj_ctrlr_state_t **cstates = enj_ctrl_get_states();
@@ -27,14 +27,42 @@ void main_mode_updater(void *data) {
     }
   }
 
+  int xpos = 50;
+  int ypos = 10;
+
+  xpos += enj_qfont_write("Expected Controllers This Run: ", xpos, ypos,
+                          PVR_LIST_TR_POLY);
+  for (int i = 0; i < MAPLE_PORT_COUNT; i++) {
+    if (1 << i & rt_state->expected_controllers) {
+      char buf[3] = {'A' + i,' ', '\0'};
+      xpos += enj_qfont_write(buf, xpos, ypos, PVR_LIST_TR_POLY);
+    }
+  }
+
+  xpos = 50;
+  ypos = 140;
+
   for (int i = 0; i < MAPLE_PORT_COUNT; i++) {
     if (((1 << i) & rt_state->expected_controllers) && !cstates[i]) {
+      enj_qfont_get_sprite_hdr()->argb = 0xffff0000;  // red for errors
       printf("Warning: Expected controller %c but not found in frame %lu!\n",
              'A' + i, rt_state->framenum);
+      char errstr[] = "Controller X missing!";
+      errstr[11] = 'A' + i;
+      enj_qfont_write(errstr, xpos, ypos, PVR_LIST_TR_POLY);
+      ypos += enj_qfont_get_header()->line_height;
     }
-
+  }
+  if (ypos == 140) {
+    enj_qfont_get_sprite_hdr()->argb = 0xff00ff00;  // green for success
+    enj_qfont_write("All expected controllers connected.", xpos, ypos,
+                    PVR_LIST_TR_POLY);
   }
   rt_state->framenum++;
+}
+
+void main_mode_updater(void *data) {
+  enj_renderlist_add(PVR_LIST_TR_POLY, render, data);
 }
 
 int main(__unused int argc, __unused char **argv) {
